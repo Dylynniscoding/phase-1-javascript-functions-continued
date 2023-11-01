e4156a971281810050ece4789c230c704c6977b7
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-11-01 12:34:32 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+
+}
+
+function mondayWork(activity="go to the office"){
+
+}
+
+function wrapAdjective(flair="*"){
+    return function(param="special"){
+        return `You are ${flair}${param}${flair}!`
+    }
+}
